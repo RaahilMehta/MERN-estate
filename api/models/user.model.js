2c2ad08662cbc6c559589a4d4a668778fc3d1ea5
RaailMehta <raahilmehta77@gmail.com>
2023-11-25 19:21:18 +0530

    creating a user model

---
+++ api/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+},{timestamps: true });
+// timestamp used to check time at which log in and update happende
+const User = mongoose.model('User', userSchema);
+export default User;
